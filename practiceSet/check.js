d4690d91f6ba9f6415bd890b89a468f5f50d36a6
rohit1620 <malavrohit1620@gmail.com>
2025-08-05 13:17:15 +0530

    add check.html

---
+++ practiceSet/check.js
@@ -0,0 +1,7 @@
+let include=document.getElementById("");
+console.log('document ji',include);
+let data=JSON.parse(localStorage.getItem("user"));
+console.log("JsonData",data);
+console.log(localStorage);
+
+
